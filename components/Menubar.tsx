bb5f1974da3d8aadff6f37e60de49f2bf4abb15a
Amritanshu Keshari <84897578+AnshuWorld@users.noreply.github.com>
2026-02-02 16:57:44 +0530

    HFJHFJ
    ,B,B,M

--- components/Menubar.tsx
+++ components/Menubar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import {Menu, X} from "lucide-react";
+import {Menu, X} from "lucide-react"; 
 import { useState } from "react";
 export default function Menubar() {
   const [isOpen, setIsOpen] = useState(false);
